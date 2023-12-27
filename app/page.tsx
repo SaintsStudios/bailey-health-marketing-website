fe04fe7e8e4703c4bfe665fc39f87ae139fc0b45
KhallilB <khallilbailey@gmail.com>
2023-12-27 10:11:45 -0800

    add todo

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Landing from '@/modules/landing/templates/Landing'
 export default function App() {
   return (
     <main>
+      {/* https://solarpunk.cool/zines/draw-with-code/line-drawing.html */}
+      {/* TODO: Background line drawing animation */}
       <Landing />
     </main>
   )
